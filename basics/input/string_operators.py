28f476fec9cf7021fb3aa50a6b02528d6a36c3c1
Viorel09 <>
2021-06-30 13:13:19 +0000

    Calculad back

---
+++ basics/input/string_operators.py
@@ -0,0 +1,7 @@
+# Demonstrating string operators
+print("Please enter the number of lives.")
+number = input()
+print("Please enter the energy level.")
+number2 = input()
+print("Please enter the shield level.")
+number3 = input()
