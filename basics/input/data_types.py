5335b0c42906ab90bdeddcd4911e93bbc2cb27b6
Viorel09 <>
2021-07-01 09:10:24 +0000

    Calculating BMI

--- basics/input/data_types.py
+++ basics/input/data_types.py
@@ -1,11 +1,19 @@
 # Calculating body mass
 print("What is your name human?")
 name = input()
+#when you use input() function the default data type is a string!
 print("How old are you (in years)?")
-age = input()
+age = int(input())
+
 print("How tall are you (in meters)?")
 height = float(input())
+
 print("How much do you weight (in kilograms)?")
-weight = int(input())
-bmi = str(weight / (height**2))
-print(name + " you are " + age + " old and your BMI is " + bmi)
+weight = float(input())
+
+bmi = weight / (height**2)
+
+#print(f"{name} you are {age} old and your BMI is {bmi}")
+#concatenation of strings (join them togheter)
+#print(name +" you are " + str(age) + " years old and your bmi is " + str(bmi))
+print("{} you are {} years old and your bmi is {:.2f}".format(name, age, bmi))
--- basics/input/string_operators.py
+++ basics/input/string_operators.py
@@ -1,7 +1,10 @@
 # Demonstrating string operators
 print("Please enter the number of lives.")
-number = input()
+number = int(input())
+while (number)
 print("Please enter the energy level.")
-number2 = input()
+number2 = int(input())
 print("Please enter the shield level.")
-number3 = input()
+number3 = int(input())
+print("Health has been set")
+print
